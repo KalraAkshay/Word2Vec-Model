5b9f11262cdce75294d1f7926c1ff53105b08dc2
KalraAkshay <43570993+KalraAkshay@users.noreply.github.com>
2020-04-26 14:10:49 +0530

    Add files via upload

---
+++ word2vec.py
@@ -0,0 +1,52 @@
+import urllib
+import bs4
+import re
+import nltk
+from gensim.models import Word2Vec
+
+source = urllib.request.urlopen("https://en.wikipedia.org/wiki/Global_warming")
+soup = bs4.BeautifulSoup(source, 'lxml')
+
+text = ""
+for paragraph in soup.find_all('p'):
+    text = text + paragraph.text
+
+text = re.sub(r"\[[0-9]*\]", " ", text)
+text = re.sub(r"\s+", " ", text)
+text = text.lower()
+text = re.sub(r"\W", " ", text)
+text = re.sub(r"\d", " ", text)
+text = re.sub(r"\s+", " ", text)
+
+# Transforming data which is acceptable by gensim word2vec model
+
+sentences = nltk.sent_tokenize(text)
+
+sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+
+# Training model
+model = Word2Vec(sentences, min_count=1)   # min_count = 1 means we are ignoring frequency less than 1
+words = model.wv.vocab                     # words used
+
+# Testing and performance of model
+# Vector Representation
+vector = model.wv['global']                 # 'global' word vector generation
+similar = model.wv.most_similar('global')    # checking similar words to global
+print(similar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
